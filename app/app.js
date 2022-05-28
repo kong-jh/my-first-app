13d4ff67f27f21f9b0915968b5e38646769630c4
Jounghun Kong <kjhun1208@khu.ac.kr>
2022-05-28 23:52:26 +0900

    Add mysql to app.js

--- app/app.js
+++ app/app.js
@@ -5,9 +5,27 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user:  'user1',
+    password:  '12345678',
+    database:  'words'
+});
+
+conn.connect();
+
 app.get('/', (req, res) => {
-    res.send('my first app');
-})
+    var sql = 'SELECT id, title, mean FROM word';
+    conn.query(sql, function(err, words){
+        if(err) {
+            console.log(err);
+            res.status(500).send('error');
+        }
+        res.send(words);
+    })
+});
 
 var server = app.listen(3000, () => {
     console.log("server listening on port 3000");
