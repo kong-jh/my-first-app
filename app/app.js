1dd346332f075b95adce2dbcc225e549219b1026
Jounghun Kong <kjhun1208@khu.ac.kr>
2022-05-29 10:58:20 +0900

    Add delete

--- app/app.js
+++ app/app.js
@@ -65,6 +65,39 @@ app.post('/:id/edit', (req, res) => {
     });
 });
 
+app.get('/:id/delete', (req, res) => {
+    var id = req.params.id;
+    var sql = 'SELECT id, title, mean FROM word';
+    conn.query(sql, function(err, words) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('error');
+        }
+        var sql = 'SELECT * FROM word WHERE id=?';
+        conn.query(sql, [id], function(err, word) {
+            if(err) {
+                console.log(err);
+                res.status(500).send('error');
+            } else {
+                res.render('delete', {words:words, word:word[0]});
+            }
+        });
+    });
+});
+
+app.post('/:id/delete', (req, res) => {
+    var id = req.params.id;
+    var sql = 'DELETE FROM word WHERE id=?';
+    conn.query(sql, [id], function(err, result) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('error');
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
 app.get(['/', '/:id'], (req, res) => {
     var sql = 'SELECT id, title, mean FROM word';
     conn.query(sql, function(err, words){
